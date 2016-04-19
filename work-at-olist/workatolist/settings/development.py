1e44b6205507a51a44c5169995f5867b1144d343
Mauricio Junior <mauricio14junior@gmail.com>
2016-04-19 04:16:12 -0300

    Prepared settings for development and production environments

---
+++ work-at-olist/workatolist/settings/development.py
@@ -0,0 +1,10 @@
+from workatolist.settings.base import *
+
+
+DEBUG = True
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'sqlite3.db'),
+    }
+}
---
+++ work-at-olist/workatolist/settings/production.py
@@ -0,0 +1,13 @@
+from workatolist.settings.base import *
+
+DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'olist',
+        'USER': 'olist',
+        'PASSWORD': 'master',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
