c1376e8c56a61d022d4fe5deae3a72e3bf9e94eb
Mauricio Junior <mauricio14junior@gmail.com>
2016-04-21 00:47:32 -0300

    Created method for the channel in which it's category_set will be overridden

--- work-at-olist/approachone/models.py
+++ work-at-olist/approachone/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from workatolist import utils
 
 class Category(models.Model):
     parent = models.ForeignKey("self", null=True, blank=True)
@@ -15,3 +15,55 @@ class Channel(models.Model):
 
     def __str__(self):
         return self.name
+
+    def override_categories(self, file_path):
+        """ Given a csv file, it will override Channel's category_set with the categories on it. """
+        flat_tree = utils.flat_tree_from_csv(file_path)
+        self.category_set.all().delete()
+        # If the first row is the column head/title it will skip the first loop iteration.
+        if len(flat_tree[0]) > 0:
+            if flat_tree[0][0] == 'Category':
+                start_loop_at = 1
+
+        inserted = []  # holds the entries already saved as categories
+        next_parent = None
+        parents = []
+        saved_categories = []
+        for i in range(start_loop_at, len(flat_tree)):
+            current_row = flat_tree[i]
+            saved_categories_row = []
+            for j in range(0, len(current_row)):
+                column = current_row[j]
+                # if the current row contains only one entry, it means it's a root node
+                if len(current_row) == 1:
+                    category = Category(name=column)
+                    category.save()
+                    self.category_set.add(category)
+                    saved_categories_row.append(category)
+                else:
+                    index_parent = current_row.index(column) - 1
+                    # Parent from this entry on the current path
+                    strparent = current_row[index_parent] if index_parent >= 0 else None
+                    path = []
+                    if strparent:
+                        path.append(strparent)
+                    path.append(column)
+                    # check if the current entry was not saved yet.
+                    if i-2 < 0 or j-1 < 0:
+                        parent = None
+                    else:
+                        parent = utils.try_catch_index(saved_categories, [i - 2, j - 1])
+
+                    if not utils.is_in(path, inserted):
+                        category = Category(name=column, parent=parent)
+                        category.save()
+                        self.category_set.add(category)
+                        saved_categories_row.append(category)
+                    else:
+                        previous_row = utils.try_catch_index(saved_categories, [i-2], [])
+                        for saved_category in previous_row:
+                            if saved_category.name == column:
+                                saved_categories_row.append(saved_category)
+                                break
+            saved_categories.append(saved_categories_row)
+            inserted.append(current_row)
---
+++ work-at-olist/workatolist/utils.py
@@ -0,0 +1,56 @@
+"""
+Utils file for helper function.
+"""
+import logging
+import csv
+# Get an instance of a logger
+logger = logging.getLogger(__name__)
+
+
+def flat_tree_from_csv(file_name):
+    """ Generates a flat tree [list] of the provided csv path. """
+    try:
+        with open(file_name) as file:
+            spamreader = csv.reader(file, delimiter='"', quotechar='|')
+            tree = []
+            for row in spamreader:
+                columns = row[0].split(',')
+                for col in columns:
+                    # In case the csv file has many columns, finds out the index of
+                    # Category
+                    if col == 'Category':
+                        category_index = columns.index(col)
+                        break
+                tree.append(columns[category_index].split(' / '))
+            return tree
+    except Exception as e:
+        print(e)
+        logger.error(e)
+
+
+def is_in(path, tree):
+    """ Given  args:
+        path<list>: ['Category 2', 'Category 3']
+        tree<list>: [
+            ['Category 1', 'Category 2', 'Category 3'],
+            ['Category 4', 'Category 5', 'Category 6'],
+        ]
+        This function will check if the given path is inside the given tree.
+    """
+    for row in tree:
+        step = len(path)
+        this_range = reversed(range(0, len(row)))
+        for i in this_range:
+            slice = row[i:i+step]
+            if ','.join(path) in ','.join(slice):
+                return True
+    return False
+
+
+def try_catch_index(listinstance, indexes, default=None):
+    for i in indexes:
+        try:
+            listinstance = listinstance.copy()[i]
+        except Exception as e:
+            return default
+    return listinstance
